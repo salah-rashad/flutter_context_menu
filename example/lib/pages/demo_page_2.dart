faceac987d5e86a80d73a6abf33eda1da776fd32
Salah Rashad <sla7.r4ad@gmail.com>
2023-11-04 07:30:39 +0200

    - added "show" method to ContextMenu widget - some improvements - updated README
    closes #1

--- example/lib/pages/demo_page_2.dart
+++ example/lib/pages/demo_page_2.dart
@@ -1,55 +1,53 @@
 part of 'demo_page.dart';
 
-final _defaultContextMenuItems = 
-   <ContextMenuEntry>[
-    DefaultContextMenuItem.submenu(
-      title: "New",
-      icon: Icons.add_rounded,
-      items: [
-        DefaultContextMenuItem(
-          title: "Node",
-          value: "Node",
-        ),
-        DefaultContextMenuItem(
-          title: "Item",
-          value: "Item",
-        ),
-        DefaultContextMenuItem(
-          title: "Group",
-          value: "Group",
-        ),
-      ],
-    ),
-    DefaultContextMenuItem(
-      title: "Open...",
-      value: "Open...",
-      icon: Icons.file_open_rounded,
-    ),
-    DefaultContextMenuItem.submenu(
-      title: "View",
-      icon: Icons.view_comfy_alt_rounded,
-      items: [
-        const DefaultContextMenuTextHeader(text: "Visibility"),
-        DefaultContextMenuItem(
-          title: "Comapct",
-          value: "Comapct",
-          icon: Icons.view_compact_rounded,
-        ),
-        DefaultContextMenuItem(
-          title: "Comfortable",
-          value: "Comfortable",
-          icon: Icons.view_comfortable_rounded,
-        ),
-        const DefaultContextMenuDivider(),
-        DefaultContextMenuItem(
-          title: "Show Mini Map",
-          value: "Show Mini Map",
-          icon: Icons.screen_search_desktop_rounded,
-        ),
-      ],
-    ),
-  ];
-
+final _defaultContextMenuItems = <ContextMenuEntry>[
+  DefaultContextMenuItem.submenu(
+    label: "New",
+    icon: Icons.add_rounded,
+    items: [
+      DefaultContextMenuItem(
+        label: "Node",
+        value: "Node",
+      ),
+      DefaultContextMenuItem(
+        label: "Item",
+        value: "Item",
+      ),
+      DefaultContextMenuItem(
+        label: "Group",
+        value: "Group",
+      ),
+    ],
+  ),
+  DefaultContextMenuItem(
+    label: "Open...",
+    value: "Open...",
+    icon: Icons.file_open_rounded,
+  ),
+  DefaultContextMenuItem.submenu(
+    label: "View",
+    icon: Icons.view_comfy_alt_rounded,
+    items: [
+      const DefaultContextMenuTextHeader(text: "Visibility"),
+      DefaultContextMenuItem(
+        label: "Comapct",
+        value: "Comapct",
+        icon: Icons.view_compact_rounded,
+      ),
+      DefaultContextMenuItem(
+        label: "Comfortable",
+        value: "Comfortable",
+        icon: Icons.view_comfortable_rounded,
+      ),
+      const DefaultContextMenuDivider(),
+      DefaultContextMenuItem(
+        label: "Show Mini Map",
+        value: "Show Mini Map",
+        icon: Icons.screen_search_desktop_rounded,
+      ),
+    ],
+  ),
+];
 
 final _customContextMenuItems = <ContextMenuEntry>[
   CustomContextMenuItem(label: "First", value: "First"),
